ca9e9c94bd34487eb2b4f41f51c88d71ad0cc1bc
TimZ99 <TimZ99@users.noreply.github.com>
2016-10-28 17:59:12 +0200

    First upload

---
+++ carousel.php
@@ -0,0 +1,72 @@
+<div id="cookies-slide" class="carousel slide" data-ride="carousel">
+<ol class="carousel-indicators">
+    <li data-target="#cookies-slide" data-slide-to="0" class="active" style="box-shadow: 0px 0px 5px #888888;"></li>
+    <li data-target="#cookies-slide" data-slide-to="1" class="" style="box-shadow: 0px 0px 5px #888888;"></li>
+    <li data-target="#cookies-slide" data-slide-to="2" class="" style="box-shadow: 0px 0px 5px #888888;"></li>
+    <li data-target="#cookies-slide" data-slide-to="3" class="" style="box-shadow: 0px 0px 5px #888888;"></li>
+    <li data-target="#cookies-slide" data-slide-to="4" class="" style="box-shadow: 0px 0px 5px #888888;"></li>
+    <li data-target="#cookies-slide" data-slide-to="5" class="" style="box-shadow: 0px 0px 5px #888888;"></li>
+    <li data-target="#cookies-slide" data-slide-to="6" class="" style="box-shadow: 0px 0px 5px #888888;"></li>
+  </ol>
+
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="img/foto1.png" alt="Cookies uitleg">
+        <div class="carousel-caption">
+        <p>Een cookies is een klein tekst bestand dat door een website op je apparaat word geplaatst.</p>
+      </div>
+    </div>
+      
+    <div class="item">
+      <img src="img/foto2.png" alt="Cookies uitleg">
+      <div class="carousel-caption">
+        <p>Stel je bezoek een website met een taal keuze.</p>
+      </div>
+    </div>
+           
+
+    <div class="item">
+      <img src="img/foto3.png" alt="Cookies uitleg">
+      <div class="carousel-caption">
+        <p>Er word een cookies aangemaakt met je taalkeuze erin.</p>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="img/foto4.png" alt="Cookies uitleg">
+      <div class="carousel-caption">
+        <p>Deze cookies word op je computer opgeslagen.</p>
+      </div>
+    </div>
+    
+        <div class="item">
+      <img src="img/foto5.png" alt="Cookies uitleg">
+      <div class="carousel-caption">
+        <p>De volgende keer dat je de website bezoekt zal de cookies herkent worden door de website.</p>
+      </div>
+    </div>
+    
+        <div class="item">
+      <img src="img/foto6.png" alt="Cookies uitleg">
+      <div class="carousel-caption">
+        <p>De website leest wat er in de cookies staat. De taal zal nu automatisch worden ingesteld.</p>
+      </div>
+    </div>
+    
+        <div class="item">
+      <img src="img/foto7.png" alt="Cookies uitleg">
+      <div class="carousel-caption">
+        <p>Een cookies is bestemd voor 1 specicfieke website. Een andere website kan deze cookie niet lezen/gebruiken.</p>
+      </div>
+    </div>
+  </div>
+
+<a class="left carousel-control" href="#cookies-slide" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+<a class="right carousel-control" href="#cookies-slide" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+</div>
---
+++ index.php
@@ -0,0 +1,172 @@
+<html lang="nl-NL"><head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cookie</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+  <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+<nav class="navbar navbar-custom navbar-fixed-top top-nav-collapse" role="navigation" style="background-color: #167FBC">
+<div class="container">
+<div class="navbar-header">
+
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+<i class="fa fa-bars" style="color:white;"></i>
+</button>
+<a class="navbar-brand page-scroll" href="#page-top" style="padding: 3px 15px;">
+<span class="light"><img class="intro-img" src="img/Cookie-nav.png" style="height:44px;"></span>
+</a>
+</div>
+ 
+<div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+<ul class="nav navbar-nav">
+ 
+<li class="hidden">
+<a href="#page-top"></a>
+</li>
+<li class="">
+<a class="page-scroll" href="#intro" style="color: #FFFFFF">Cookies?</a>
+</li>
+<li class="">
+<a class="page-scroll" href="#geschiedenis" style="color: #FFFFFF">Geschiedenis</a>
+</li>
+<li class="">
+<a class="page-scroll" href="#wet" style="color: #FFFFFF">Cookie wet</a>
+</li>
+</ul>
+</div>
+</div>
+</nav>
+
+<section id="intro" class="container content-section">
+<div class="row">
+<div class="col-lg-8 col-lg-offset-2">
+<br>
+<br>
+<br>
+<div class="alert alert-warning alert-dismissible" role="alert" style="margin-top:5px">
+  <button type="button" id="closer" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  <!--<b>Cookie Warning!</b> Deze website maakt<b>geen</b> gebruik van cookies. Dit hoeven we niet te melden. Toch doen we het. Een website over cookies moet ook een waarschuwing over cookies hebben.-->
+<b>Cookie Warning!</b> Deze website maakt gebruik van cookies. Bij het gebruik van deze website gaat u er mee akkoord dat er cookies worden geplaatst voor het gebruik van de service AdSense™. <a href="http://www.google.nl/intl/nl/policies/technologies/ads/">Meer informatie.</a>
+</div>
+<h2>Wat zijn cookies?</h2>
+<div class="row">
+<div class="col-xs-12 col-sm-6 col-lg-6">
+<p style="text-align: justify;">Een cookie is een klein tekstbestandje dat een website op je apparaat zet zet op het moment dat je de site bezoekt. De belangrijkste functie van cookies is om de ene gebruiker van de andere te onderscheiden. Je komt cookies dan ook veel tegen bij websites waarbij je moet inloggen. Een cookie zorgt er dan voor dat je ingelogd blijft terwijl je de site gebruikt. </p>
+<br>
+<b>Websites die veel cookies bevatten:</b>
+<br>
+- TVGids met 71 cookies waarvan 68 van derde<br>
+- Telegraaf met 52 cookies waarvan 43 van derde<br>
+- Sunweb met 40 cookies waarvan 29 van derde<br>
+- NOS met 36 cookies waarvan 31 van derde<br>
+- RTL met 30 cookies waarvan 29 van derde
+<br>
+<br>
+<b>Cookies van derden</b><br>
+Sommige websites staan toe dat andere websites ook cookies kunnen plaatsen op je apparaat. Dit soort cookies noemen we cookies van derden (3rd party cookies).
+<br>
+<br>
+</div>
+<div class="col-xs-12 col-sm-6 col-lg-6">
+<br>
+<?php include 'carousel.php'; ?>
+</div>
+<div class="col-xs-12 col-sm-12 col-lg-12">
+<b>Cookies verwijderen</b>
+<div class="row">
+<div class="col-xs-12 col-sm-6 col-lg-6">
+Cookies verwijderen in <b>Internet Explorer</b><br><br>
+Ga naar het menu Extra > Internetopties > tab Algemeen. Klik onder Browsegeschiedenis op Verwijderen.
+Je wilt misschien niet alles verwijderen. Zorg dat in ieder geval Cookies aangevinkt staat en klik op Verwijderen.
+<br><br><br>
+Cookies verwijderen in <b>Firefox</b><br><br>
+Klik op het blokje met 3 streepjes rechtsbovenin > klik op 'Opties' > Selecteer 'individuele cookies verwijderen'.
+Klik op 'Alle cookies verwijderen'.
+<br><br>
+</div>
+<div class="col-xs-12 col-sm-6 col-lg-6">
+Cookies verwijderen in <b>Chrome</b><br>
+<br>
+Klik op het blokje met 3 streepjes, rechtsbovenin. Klik op Geschiedenis > Browsegeschiedenis wissen. Klik onder Privacy op Browsegegevens wissen...
+Zorg in ieder geval dat Cookies en andere siteplugingegevens aangevinkt staat bij het verwijderen.
+<br><br>Cookies verwijderen in <b>Safari</b><br>
+<br>
+Klik op 'Safari' boven in. Klik op 'Voorkeuren' > 'Privacy'. Klik op 'Verwijder alle websitegegevens'.
+</div>
+</div></div>
+
+</div>
+</div>
+</div>
+</section>
+
+<section id="geschiedenis" class="container content-section">
+<div class="row">
+<div class="col-lg-8 col-lg-offset-2">
+<br><br>
+<h2>Geschiedenis</h2>
+<p style="text-align: justify;">
+Cookies werden voor het eerst geïntroduceerd in de browser Netscape 1.0. Een andere browser, Internet Explorer ondersteunde cookies pas vanaf versie 2.0. Pas in Netscape 3 liet Lou Montulli een optie inbouwen waardoor internetgebruikers de cookies konden zien en waarmee je gewaarschuwd kon worden als er cookies geplaatst probeerde te worden. In de tussentijd lag de advertentie industrie ook niet onder een steen en begonnen ze cookies te gebruiken om een profiel van iemand te maken.
+<br>
+In 1997 kwam er een voorstel dat browsermakers aanmoedigde om het gebruik van cookies overzichtelijk te maken. Je kon hierdoor instellen in welke mate je cookies wil accepteren. Hiermee kan bijvoorbeeld profilering tegen worden gegaan. </p>
+<p><br></p>
+</div>
+</div>
+</section>
+
+<section id="geschiedenis" class="container content-section">
+<div class="row">
+<div class="col-lg-8 col-lg-offset-2">
+<br><br>
+<hr>
+<h4 style="text-align: center;">Sorry, een Advertentie :(</h4>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- AD1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-8333217313626330"
+     data-ad-slot="5147689600"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+<hr>
+</div>
+</div>
+</section>
+
+<section id="wet" class="container content-section">
+<div class="row">
+<div class="col-lg-8 col-lg-offset-2">
+<br><br>
+<h2>Nederlandse cookie wetgeving</h2>
+<p style="text-align: justify;">
+Sinds 2012 is Artikel 11.7a van de Telecommunicatiewet van kracht. Artikel 11.7a van de Telecommunicatiewet beter bekend als de cookiewet houd in dat elke website die bezoekers vanuit Nederland trekt de bezoeker moet informeren over welke cookies er worden geplaatst op zijn of haar computer en waar deze voor dienen. Dit kan via een privacystatementpagina. Naast deze informatie moet elke bezoeker vanuit Nederland éénmalig toestemming geven voor het plaatsen van cookies. Deze toestemming kan gevraagd worden via een pop-up of een balk boven aan de pagina met een “Ik geef toestemming” knop.
+<br>
+Op 11 maart 2015 heeft de Nederlandse regering de Telecommunicatiewet aangepast. Websites hebben nu geen toestemming meer nodig voor het plaatsen van analytische cookies.
+Analytische cookies zijn cookies die bijvoorbeeld worden gebruikt om bezoekersaantallen bij te houden. Deze cookies hebben nauwelijks gevolgen voor de privacy.
+<br>
+Naast analytische cookies hoef je voor functionele cookies ook geen toestemming te vragen. Onder functionele cookies vallen cookies die onder andere worden gebruik om bij te houden wat er in een winkelmandje zit. Dit zijn heel vaak session cookies. Na een bepaalde tijd worden deze verwijderd. Deze cookies zijn nodig om de dienst te laten werken.
+Tracking cookies, deze cookies vallen in de categorie; toestemming vereist. Met deze cookies word inbreuk op je privacy gedaan. Deze cookies worden onder andere gebruikt om gerichte advertenties te laten zien aan je. Er word een profiel gemaakt (profiling).
+</p>
+<p><br></p>
+</div>
+</div>
+</section>
+<footer>
+<div class="container text-center">
+Adsense™ is een handelsmerk van Google Inc.<br>Copyright © 2015-2016 Tim || Alle rechten voorbehouden. <a href="http://p10691.hageveld2.nl/per51eind/privacy.php">Privacy policy</a><br></div>
+</footer>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/overig.js"></script>
+</body></html>
