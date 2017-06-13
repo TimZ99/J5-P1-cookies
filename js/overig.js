8e34a6cffff17de06639eaf7f1d5e73b87c15827
Timz99 <TimZ99@users.noreply.github.com>
2017-06-14 01:02:20 +0200

    ' vervangen door "

--- js/overig.js
+++ js/overig.js
@@ -1,9 +1,9 @@
 $(function() {
-    $("a.page-scroll").bind('click', function(event) {
+    $("a.page-scroll").bind("click", function(event) {
         var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        $("html, body").stop().animate({
+            scrollTop: $($anchor.attr("href")).offset().top
+        }, 1500, "easeInOutExpo");
         event.preventDefault();
     });
 });
